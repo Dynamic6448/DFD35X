8f82e3e049173958b2d61abf4037f19640f7457f
zackprogrammer <63200308+zackprogrammer@users.noreply.github.com>
2021-03-25 12:54:45 -0400

    install react and other stuff

--- install.bat
+++ install.bat
@@ -1,1 +1,5 @@
+echo "u need to install npm and this is a windows only script. To install npm u need to install node.js. If u still get an error u have not installed this properly."
+
+npm install -g create-react-app
+
 
